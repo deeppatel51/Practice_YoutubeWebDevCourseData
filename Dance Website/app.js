7194ac90647e1bb885afb3cbe23b664901da7db9
Deep Patel <88723958+deeppatel51@users.noreply.github.com>
2022-08-19 14:28:37 +0530

    Update app.js

--- Dance Website/app.js
+++ Dance Website/app.js
@@ -2,8 +2,25 @@ const express = require("express");
 const app= express();
 const path = require("path");
 const fs = require("fs");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true});
+}
 const port=80;
 
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String
+  });
+
+  const contact = mongoose.model('Contact', contactSchema);
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
@@ -21,6 +38,15 @@ app.get('/', (req, res)=>{
 app.get("/contact", (req, res)=>{ 
     const params = { }
     res.status(200).render('contact.pug', params);
+})
+app.get("/contact", (req, res)=>{ 
+    var myData = new Contact(req.body);
+    myData.save().then(() =>{
+        res.send("this item has been send to the database")
+    }).catch(()=>{
+        res.status(400).send("item was not saved to the database") 
+    });
+    // res.status(200).render('contact.pug');
 });
 
 // START THE SERVER
